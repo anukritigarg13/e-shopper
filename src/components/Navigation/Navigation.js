6953f6c061c50940cd527f44449d063b9559d7b7
Anukriti <anukritigarg13@gmail.com>
2021-03-05 09:29:16 +0530

    fix(navigation): Add styling for nav links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,9 +6,9 @@ import { Link } from 'react-router-dom';
 const Navigation = (props) => (
   <div className="navigation">
 
-    <Link to="/"><span>E-Shopper</span></Link>
+    <Link to="/"><span className="navigation-links">E-Shopper</span></Link>
     <Link to="/cart">
-      <span>
+      <span className="navigation-links">
         My Basket :
         {props.cartItemsCount}
       </span>
