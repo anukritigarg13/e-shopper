bb9874ef102ebe600c1fa48e74c8bee89c9ecd4a
Anukriti <anukritigarg13@gmail.com>
2021-03-08 11:44:51 +0530

    feat(checkout): Change into functional component with react hooks

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,72 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Checkout.css';
 import validator from 'validator';
 
-class Checkout extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: {
-        name: '',
-        contactNo: undefined,
-        email: '',
-        address: '',
-      },
-      error: null,
+const Checkout = () => {
+  const [name, setName] = useState('');
+  const [contactNo, setContactNo] = useState(undefined);
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
 
-    };
-  }
-
-  onChangeHandler = (event, field) => {
+  const onChangeHandler = (event, field) => {
     const newValue = event.target.value;
     if (field === 'contactNo' && newValue.length > 10) {
-      this.setState((prevState) => ({ ...prevState }));
       return;
     }
-    this.setState((prevState) => ({
-      ...prevState,
-      data: { ...prevState.data, [field]: newValue },
-    }));
-  }
+    switch (field) {
+      case ('name'):
+        setName(newValue);
+        break;
+      case ('contactNo'):
+        setContactNo(newValue);
+        break;
+      case ('address'):
+        setAddress(newValue);
+        break;
+      case ('email'):
+        setEmail(newValue);
+        break;
+      default:
+    }
+  };
 
-  onCheckoutHandler = () => {
-    const { data } = this.state;
-    const {
-      email, contactNo, name, address,
-    } = data;
+  const onCheckoutHandler = () => {
     if (!validator.isEmail(email)
      || name.length === 0 || address.length === 0 || !contactNo || contactNo.length < 10) {
       alert('Please enter valid details');
     }
-  }
+  };
 
-  render() {
-    const { data } = this.state;
-    const {
-      name, address, contactNo, email,
-    } = data;
-    return (
-      <div className="checkout">
-        <div className="form">
-          <form>
-            Name
-            <input type="text" onChange={(event) => this.onChangeHandler(event, 'name')} value={name} />
-            Address
-            <input type="text" onChange={(event) => this.onChangeHandler(event, 'address')} value={address} />
-            Contact Number
-            <input type="number" onChange={(event) => this.onChangeHandler(event, 'contactNo')} value={contactNo} />
-            E-mail
-            <input type="text" onChange={(event) => this.onChangeHandler(event, 'email')} value={email} />
-          </form>
-        </div>
-        <div>
-          <button onClick={this.onCheckoutHandler} type="button">Submit</button>
-        </div>
+  return (
 
+    <div className="checkout">
+      <div className="form">
+        <form>
+          Name
+          <input type="text" onChange={(event) => onChangeHandler(event, 'name')} value={name} />
+          Address
+          <input type="text" onChange={(event) => onChangeHandler(event, 'address')} value={address} />
+          Contact Number
+          <input type="number" onChange={(event) => onChangeHandler(event, 'contactNo')} value={contactNo} />
+          E-mail
+          <input type="text" onChange={(event) => onChangeHandler(event, 'email')} value={email} />
+        </form>
+      </div>
+      <div>
+        <button onClick={onCheckoutHandler} type="button">Submit</button>
       </div>
 
-    );
-  }
-}
+    </div>
+
+  );
+};
 
 export default Checkout;
