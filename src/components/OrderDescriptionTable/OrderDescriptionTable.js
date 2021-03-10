6dc4caa8c1f50765aae12b6044983bf1be42db45
Anukriti <anukritigarg13@gmail.com>
2021-03-10 22:12:15 +0530

    fix(e-shopper): Add only categories that are in basket in the cart table

--- src/components/OrderDescriptionTable/OrderDescriptionTable.js
+++ src/components/OrderDescriptionTable/OrderDescriptionTable.js
@@ -3,25 +3,28 @@ import PropTypes from 'prop-types';
 import './OrderDescriptionTable.css';
 
 const OrderDescTable = ({ products }) => {
-  const itemsInBasket = products.map((product) => {
-    const {
-      itemName, itemCount, unitPrice, unitQuantity, companyName,
-    } = product;
-    return (
-
-      <tr key={product.id}>
-        <td>
-          {itemName}
-          {' '}
-          {companyName}
-          {' '}
-          {unitQuantity}
-        </td>
-        <td>{unitPrice}</td>
-        <td>{itemCount}</td>
-        <td>{unitPrice * itemCount}</td>
+  const categoriesInCart = Object.keys(products).filter((category) => {
+    const categoryExistInCart = products[category].find((product) => product.itemCount > 0);
+    if (categoryExistInCart) {
+      return true;
+    }
+    return false;
+  });
+  console.log(categoriesInCart);
+  const itemsInCart = categoriesInCart.map((productCategory) => {
+    const categoryItemsInCart = products[productCategory].map((product) => (
+      <tr>
+        <td>{product.name}</td>
+        <td>{product.price}</td>
+        <td>{product.itemCount}</td>
+        <td>{product.itemCount * product.price}</td>
       </tr>
-
+    ));
+    return (
+      <>
+        <tr><td className="category-row" colSpan="4">{productCategory}</td></tr>
+        {categoryItemsInCart}
+      </>
     );
   });
   return (
@@ -35,20 +38,21 @@ const OrderDescTable = ({ products }) => {
         </tr>
       </thead>
       <tbody>
-        {itemsInBasket}
+        {itemsInCart}
       </tbody>
     </table>
   );
 };
-const productTableShape = {
-  itemName: PropTypes.string.isRequired,
-  itemCount: PropTypes.number.isRequired,
-  unitPrice: PropTypes.number.isRequired,
-  unitQuantity: PropTypes.string.isRequired,
-  companyName: PropTypes.string.isRequired,
-};
-OrderDescTable.propTypes = {
-  products: PropTypes.arrayOf(PropTypes.shape(productTableShape)).isRequired,
-};
 
+const productsShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  itemCount: PropTypes.number.isRequired,
+  count: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
+});
+OrderDescTable.propTypes = {
+  products: PropTypes.objectOf(PropTypes.arrayOf(productsShape)).isRequired,
+};
 export default OrderDescTable;
