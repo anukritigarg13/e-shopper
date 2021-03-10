379b5064909e7adf8f0493b5650dd9af1d77aad7
Anukriti <anukritigarg13@gmail.com>
2021-03-10 16:56:59 +0530

    fix(product): Color out of stock product grey

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -6,31 +6,36 @@ import './Product.css';
 const Product = ({
   imgSrc, imgAlt, companyName, itemName, itemCount, unitQuantity, unitPrice,
   add, remove, stock,
-}) => (
-  <div className="product" data-testid="product-test">
-    <div className="product-image-div">
-      <img data-testid="product-img" className="product-image" src={imgSrc} alt={imgAlt} />
-    </div>
+}) => {
+  const quantityControl = stock ? (
+    <QuantityControl
+      add={add}
+      remove={remove}
+      quantity={itemCount}
+    />
+  ) : <p>Out of stock</p>;
+  return (
+    <div className={stock ? 'product' : 'out-of-stock'} data-testid="product-test">
+      <div className="product-image-div">
+        <img data-testid="product-img" className="product-image" src={imgSrc} alt={imgAlt} />
+      </div>
 
-    <p>{companyName}</p>
-    <p>{itemName}</p>
-    <p>{unitQuantity}</p>
-    <div className="quantity-placard">
-      <p>
-        {stock}
-        MRP
-        {' '}
-        {unitPrice}
-        /-
-      </p>
-      <QuantityControl
-        add={add}
-        remove={remove}
-        quantity={itemCount}
-      />
+      <p>{companyName}</p>
+      <p>{itemName}</p>
+      <p>{unitQuantity}</p>
+      <div className="quantity-placard">
+        <p>
+          {stock}
+          MRP
+          {' '}
+          {unitPrice}
+          /-
+        </p>
+        {quantityControl}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Product.propTypes = {
   imgAlt: PropTypes.string.isRequired,
